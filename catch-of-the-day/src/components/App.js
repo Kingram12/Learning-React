6e790c060dd0afa5364cb5c0135fb03b9b2545cc
Kayla Ingram <kaylabsi@outlook.com>
2021-01-08 14:41:58 -0500

    added fish form

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import Header from './Header';
-import Order from './Order';
-import Inventory from './Inventory';
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
 
 class App extends React.Component {
-render() {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // 1. Take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // 2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set the new fishes object to state
+    this.setState({ fishes });
+  };
+  render() {
     return (
-        <div className="catch-of-the-day">
+      <div className="catch-of-the-day">
         <div className="menu">
-            <Header tagline="Fresh Seafood Market"></Header>
+          <Header tagline="Fresh Seafood Market" />
         </div>
-        <Order></Order>
-        <Inventory></Inventory>
-        </div>
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
     );
-}
+  }
 }
 
 export default App;
-
 
 //export right away - don't forget it
 //anything that isnt a string is passed in via curly braces
--- catch-of-the-day/src/components/Router.js
+++ catch-of-the-day/src/components/Router.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import StorePicker from "./StorePicker"
-import App from "./App"
-import NotFound from "./NotFound"
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import StorePicker from "./StorePicker";
+import App from "./App";
+import NotFound from "./NotFound";
 
 const Router = () => (
-    <BrowserRouter>
+  <BrowserRouter>
     <Switch>
-        <Route exact path="/" component={StorePicker}></Route>
-        <Route exact path="/store/:storeId" component={App}></Route>
-        <Route component={NotFound}></Route>
+      <Route exact path="/" component={StorePicker} />
+      <Route path="/store/:storeId" component={App} />
+      <Route component={NotFound} />
     </Switch>
-    </BrowserRouter> );
+  </BrowserRouter>
+);
 
-    export default Router;
+export default Router;
